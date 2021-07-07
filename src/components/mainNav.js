cb8e19681afe2932d58835e1d0b5661d2630dc97
Andrew Fleming <66144906+AndrewJFleming@users.noreply.github.com>
2021-07-07 15:35:39 -0700

    Add nested menu item support
    -Make top-menu support nested menu items.
    -Previous mainNav component saved as backup.

--- src/components/mainNav.js
+++ src/components/mainNav.js
@@ -1,49 +1,67 @@
-/**
- * Creates hierarchical menu based on WordPress menu.
- * @link https://www.wpgraphql.com/docs/menus/#hierarchical-data
- */
  import React from "react"
  import { useStaticQuery, graphql } from "gatsby"
- 
  import UniversalLink from "../utils/UniversalLink"
+ import { FlatListToHierarchical } from "../utils/FlatListToHierarchical"
+ 
  import * as style from "./mainNav.module.css"
+
+ const MenuLoop = ({ menuItems }) => {
+   return (
+     <ul>
+       {menuItems.map((menuItem, index) => {
+         return (
+           <li
+             key={index}
+             className={menuItem.routes.length > 0 ? "has-submenu" : undefined}
+           >
+             <UniversalLink to={menuItem.path} activeClassName="current-page">
+               {menuItem.title}
+             </UniversalLink>
+             {menuItem.routes.length > 0 && (
+               <MenuLoop menuItems={menuItem.routes}></MenuLoop>
+             )}
+           </li>
+         )
+       })}
+     </ul>
+   )
+ }
  
  const MainNav = () => {
    const wpMenu = useStaticQuery(graphql`
      {
        allWpMenuItem(
+         sort: { fields: order, order: ASC }
          filter: {
            menu: { node: { slug: { eq: "top-menu" } } }
-           parentDatabaseId: { eq: 0 }
+          #  parentDatabaseId: { eq: 0 }
          }
        ) {
          nodes {
+           id
            title: label
            path
+           target
+           parent: parentId
          }
        }
      }
    `)
  
-    /* For flat menu only; Hierarchical menus work differently */
-   const menuItems = wpMenu.allWpMenuItem.nodes
+   console.log("Raw data:", wpMenu.allWpMenuItem.nodes)
+   const headerMenu = FlatListToHierarchical(wpMenu.allWpMenuItem.nodes, {
+     idKey: "id",
+     childrenKey: "routes",
+     parentKey: "parent",
+   })
+   console.log("headerMenu: ", headerMenu)
  
-    return (
-        <nav className={style.mainnav}>
-            <ul>
-                {menuItems.map((menuItem, index) => {
-                    return (
-                        <li key={index}>
-                            <UniversalLink to={menuItem.path} activeClassName="current-page">
-                                {menuItem.title}
-                            </UniversalLink>
-                        </li>
-                    )
-                })}
-            </ul>
-        </nav>
-    )
-}
+   return (
+     <nav className={style.mainnav}>
+       {headerMenu.length > 0 && <MenuLoop menuItems={headerMenu}></MenuLoop>}
+     </nav>
+   )
+ }
  
  export default MainNav
  
